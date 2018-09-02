e59ea24e204d7d2e91ab25805e7a4368b2e3d63a
darkobits <joshua@frontlawn.net>
2018-09-02 16:41:02 -0700

    fix: Handle absolute paths correctly.

--- src/import-unique.ts
+++ src/import-unique.ts
@@ -14,7 +14,11 @@ export default function importUnique(name: string): any {
 
   // Compute the path Node would have used to resolve the module had it been
   // imported from the caller's file.
-  const key = resolveFrom(path.dirname(callerPath()), name);
+  const callPath = callerPath();
+
+  // If callerPath() returns null, we are likely trying to import a module using
+  // an absolute path, so just use the provided "name".
+  const key = callPath ? resolveFrom(path.dirname(callPath), name) : name;
 
   // Save a copy of the cached module, because we just might need it later.
   const cached = require.cache[key];
